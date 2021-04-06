ee6478b7af4b52d3b306d348777aed1cab161ccb
Kyle Hickinson <git@kylehickinson.com>
2021-04-06 15:22:23 -0400

    Increase size of menu popover on iPad

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -2509,6 +2509,7 @@ extension BrowserViewController: ToolbarDelegate {
         presentPanModal(menuController, sourceView: tabToolbar.menuButton, sourceRect: tabToolbar.menuButton.bounds)
         if menuController.modalPresentationStyle == .popover {
             menuController.popoverPresentationController?.popoverLayoutMargins = .init(equalInset: 4)
+            menuController.popoverPresentationController?.permittedArrowDirections = [.up]
         }
     }
     
